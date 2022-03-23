5cb5e76e4a67156f0910b9d1ba98e2a2969d4007
Andreea <andreea.ioana.moldoveanu@gmail.com>
2022-03-23 16:04:36 +0200

    1st commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello");
+        System.out.println("Andreea");
+
+        int a = 5;
+        int b = 7;
+        int total = a + b;
+        System.out.println("the result is " + total);
+
+        int x = 11;
+        int y = 4;
+        double divided = x / y;
+        System.out.println("the result is " + divided);
+
+        int c = -5;
+        int d = 8;
+        int e = 6;
+        double sum1 = d + c * e;
+        System.out.println("the result is " + sum1);
+
+        int f = 55;
+        int g = 9;
+        double sum2 = (f+g) % g;
+        System.out.println("the result is " + sum2);
+
+        int i = 20;
+        int j = -3;
+        int k = 5;
+        int l = 8;
+        double sum3 = i+j*k/l;
+        System.out.println("the result is " + sum3);
+
+        int m = 5;
+        int n = 15;
+        int o = 3;
+        int p = 2;
+        int q = 8;
+        double sum4 = m+n/o*p-q%o;
+        System.out.println("te result is " +sum4);
+    }
+}
